37fc3d4a1b0a261e48fab01afabbe92e43849705
Gustavo Moreira <g.hen.moreira@gmail.com>
2025-03-29 23:31:08 -0300

    feat: producer basics files

---
+++ producer_bk/main.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+# Criando a instância do Flask
+app = Flask(__name__)
+
+# Definindo uma rota simples
+@app.route('/')
+def hello_world():
+    return 'Olá, Mundo!'
+
+# Iniciando o servidor
+if __name__ == '__main__':
+app.run(debug=True, port=5002)
